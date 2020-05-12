b3879e991853e29780db4815241b27472c5217d1
Kevin Kuang <kvnkuang@users.noreply.github.com>
2020-05-12 17:00:51 -0400

    Finished onbarding section 2

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <Navbar />
     <Index />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
 import Index from './Index.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
     Navbar,
-    Index
+    Index,
+    Footer
   }
 }
 </script>
--- src/Index.vue
+++ src/Index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="content">
+  <section>
     <!-- Landing section-->
     <section class="hero is-large is-primary is-bold">
       <div class="hero-body">
@@ -103,8 +103,146 @@
     </section>
 
     <!-- Onboarding section 2 -->
-  </div>
+    <section class="hero is-medium is-light is-bold">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title is-uppercase">
+            Why register your MAVE activity?
+          </h1>
+
+          <div class="section">
+            <b-tabs
+              v-model="activeTab"
+              size="is-small"
+              position="is-centered"
+              class="block"
+            >
+              <b-tab-item
+                v-for="(tab, index) in tabs"
+                :key="index"
+              >
+                <!-- Custom tab header -->
+                <template slot="header">
+                  <div class="content has-text-centered">
+                    <div
+                      class="step-number is-inline-flex has-vcentered"
+                      :class="{ 'has-background-primary has-text-white': activeTab == index, 'has-text-primary': activeTab != index }"
+                    >
+                      <p
+                        class="has-fullwidth"
+                      >
+                        <i
+                          class="mdil"
+                          :class="tab.icon"
+                        />
+                      </p>
+                    </div>
+                    <p
+                      v-if="tab.title"
+                      class="tab-title is-size-5 has-text-primary"
+                    >
+                      {{ tab.title }}
+                    </p>
+                  </div>
+                </template>
+
+                <!-- Tab content -->
+                <div class="container has-text-left is-flex has-vcentered">
+                  <div>
+                    <img
+                      :src="tab.image"
+                      :alt="tab.image_alt_text"
+                    >
+                  </div>
+                  <div class="tab-description">
+                    <div class="card">
+                      <div class="card-content">
+                        <div class="content">
+                          <h2 class="subtitle has-text-primary">
+                            {{ tab.title }}
+                          </h2>
+                        </div>
+                        <div class="columns">
+                          <div class="column is-narrow">
+                            <b-icon
+                              icon="mdil-check"
+                              custom-size="mdil-36px"
+                              type="is-primary"
+                            />
+                          </div>
+                          <div class="column">
+                            <p>{{ tab.description }}</p>
+                          </div>
+                        </div>
+                        <div>
+                          <b-button
+                            tag="router-link"
+                            :to="tab.action_link"
+                            size="is-medium"
+                            class="is-capitalized"
+                            type="is-primary"
+                            expanded
+                          >
+                            {{ tab.action_text }}
+                          </b-button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </b-tab-item>
+            </b-tabs>
+          </div>
+        </div>
+      </div>
+    </section>
+  </section>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      activeTab: 0,
+      tabs: [
+        {
+          icon: "mdil-bell",
+          title: "Stay in the Loop",
+          description: "Registered members can access other teams' and data (when available) on the targets you have followed.",
+          image: "https://bulma.io/images/placeholders/640x360.png",
+          image_alt_text: "Placeholder image",
+          action_text: "Register activity",
+          action_link: "#"
+        },
+        {
+          icon: "mdil-vector-union",
+          title: "Foster Collaboration",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vehicula quam ipsum, ut iaculis mi molestie elementum.",
+          image: "https://bulma.io/images/placeholders/640x360.png",
+          action_text: "Register activity",
+          action_link: "#"
+        },
+        {
+          icon: "mdil-shuffle",
+          title: "Resolve Conflict",
+          description: "Morbi ut rhoncus augue. Sed nec dui a enim feugiat lacinia id et turpis. Mauris faucibus semper molestie.",
+          image: "https://bulma.io/images/placeholders/640x360.png",
+          action_text: "Register activity",
+          action_link: "#"
+        },
+        {
+          icon: "mdil-currency-usd",
+          title: "Seek Funding",
+          description: "In lobortis laoreet magna et sodales. Cras mi tellus, egestas ut pretium commodo,",
+          image: "https://bulma.io/images/placeholders/640x360.png",
+          action_text: "Register activity",
+          action_link: "#"
+        }
+      ]
+    }
+  }
+}
+</script>
 
 <style lang="sass" scoped>
 @import "@/assets/variables.sass"
@@ -127,5 +265,10 @@
   font-weight: lighter
   border-radius: 50%
   border: 1px solid $primary
-  background-color: white
+.tab-title
+  padding: 1rem
+.tab-description
+  width: 50%
+  position: absolute
+  right: 0px
 </style>
