79a96bd4e88a5ed520c9c2df4d24413e746c812e
Kevin Kuang <kvnkuang@users.noreply.github.com>
2020-05-17 21:49:23 -0400

    Add login and signup UI

--- src/App.vue
+++ src/App.vue
@@ -55,11 +55,152 @@
       <!-- Right side: user functions -->
       <template slot="end">
         <b-navbar-item tag="div">
-          <b-button>Log in</b-button>
+          <b-button 
+            type="is-primary" 
+            @click="isLoginSignupModalActive = true"
+          >
+            Log in
+          </b-button>
         </b-navbar-item>
       </template>
     </b-navbar>
 
+    <!-- Log in panel -->
+    <b-modal
+      :active.sync="isLoginSignupModalActive"
+      :width="500"
+      @close="cleanupLoginSignup()"
+    >
+      <div class="card">
+        <div class="card-content">
+          <div class="section">
+            <div class="content has-text-centered">
+              <h2 class="subtitle">
+                MAVE Registry Logo
+              </h2>
+            </div>
+
+            <!-- Log in/Sign up tab -->
+            <b-tabs
+              position="is-centered"
+              type="is-boxed"
+              expanded
+              :animated="false"
+            >
+              <!-- Log in tab -->
+              <b-tab-item
+                icon="mdil-login"
+                label="Log in"
+              >
+                <b-field>
+                  <b-input
+                    type="email"
+                    icon="mdil-email"
+                    placeholder="Email"
+                  />
+                </b-field>
+                <b-field>
+                  <b-input
+                    type="password"
+                    icon="mdil-lock"
+                    placeholder="Password"
+                  />
+                </b-field>
+                <p class="control">
+                  <b-button
+                    expanded
+                    class="is-primary"
+                  >
+                    Log in
+                  </b-button>
+                </p>
+                <p class="forget-password has-text-right">
+                  Forget Password?
+                </p>
+                <div class="level">
+                  <div class="level-left">
+                    <p class="has-text-weight-medium has-text-centered">
+                      Log in with
+                    </p>
+                  </div>
+                  <div class="level-right oauth">
+                    <div class="buttons is-space-between">
+                      <b-button
+                        icon-pack="mdi"
+                        icon-left="google"
+                        type="is-light"
+                      >
+                        Google
+                      </b-button>
+                      <b-button
+                        icon-pack="fab"
+                        icon-left="orcid"
+                        type="is-light"
+                      >
+                        ORCID
+                      </b-button>
+                    </div>
+                  </div>
+                </div>
+              </b-tab-item>
+              <!-- Sign up tab -->
+              <b-tab-item
+                icon="mdil-file-plus"
+                label="Sign up"
+              >
+                <b-field>
+                  <b-input
+                    type="email"
+                    icon="mdil-email"
+                    placeholder="Email"
+                  />
+                </b-field>
+                <b-field>
+                  <b-input
+                    type="password"
+                    icon="mdil-lock"
+                    placeholder="Password"
+                  />
+                </b-field>
+                <p class="control">
+                  <b-button
+                    expanded
+                    class="is-primary"
+                  >
+                    Sign up
+                  </b-button>
+                </p>
+                <div class="level oauth signup">
+                  <div class="level-left">
+                    <p class="has-text-weight-medium has-text-centered">
+                      Sign up with
+                    </p>
+                  </div>
+                  <div class="level-right oauth">
+                    <div class="buttons is-space-between">
+                      <b-button
+                        icon-pack="mdi"
+                        icon-left="google"
+                        type="is-light"
+                      >
+                        Google
+                      </b-button>
+                      <b-button
+                        icon-pack="fab"
+                        icon-left="orcid"
+                        type="is-light"
+                      >
+                        ORCID
+                      </b-button>
+                    </div>
+                  </div>
+                </div>
+              </b-tab-item>
+            </b-tabs>
+          </div>
+        </div>
+      </div>
+    </b-modal>
     <!-- Component injection -->
     <router-view />
 
@@ -79,8 +220,41 @@
 </template>
 
 <style lang="sass" scoped>
+@import "@/assets/variables.sass"
+
+.oauth
+  @media screen and (max-width: $break-mobile)
+    margin-top: 1rem !important
+  &.signup
+    margin-top: 1rem
 .footer
   padding: 3rem 1.5rem
 .navbar-brand .is-active // Remove active style for the logo
   background-color: transparent !important
+.forget-password
+  padding: 0.25rem 0 1rem 0
 </style>
+
+<script>
+export default {
+  data () {
+    return {
+      isLoginSignupModalActive: false,
+      loginSignupProp: {
+        email: "",
+        password: "",
+        method: ""
+      }
+    }
+  },
+  methods: {
+    cleanupLoginSignup() {
+      this.loginSignupProp = {
+        email: "",
+        password: "",
+        method: ""
+      }
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -11,15 +11,20 @@ import 'buefy/dist/buefy.css'
 import '@mdi/font/css/materialdesignicons.css'
 import '@mdi/light-font/css/materialdesignicons-light.css'
 import '@/assets/styles.sass'
+import '@fortawesome/fontawesome-free/css/brands.css'
+import '@fortawesome/fontawesome-free/css/fontawesome.css'
+
+// Import custom scripts
 import router from './router'
+
+// Initialize plugins
 
 // Initialize frameworks
 Vue.use(Buefy, {
   defaultIconPack: 'mdil',
 })
 
-// Initialize plugins
-
+// Initialize parameters
 Vue.config.productionTip = false
 
 new Vue({
