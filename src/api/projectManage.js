046b24ae77c663fda3a2a28cbf4332475e62f00b
Kevin Kuang <kvnkuang@users.noreply.github.com>
2020-06-25 14:39:49 -0400

    Abstract project activity in a new class

--- src/api/projectManage.js
+++ src/api/projectManage.js
@@ -133,6 +133,7 @@ export const Project = Parse.Object.extend("Project", {
     // Access project progress detail
     if (detail) {
       ret.leads = this.get("leads")
+      ret.funding = this.get("funding")
 
       // Format team
       const team = this.get("team")
@@ -142,8 +143,9 @@ export const Project = Parse.Object.extend("Project", {
       const collaborators = this.get("collaborators")
       if (collaborators && collaborators.length > 0) ret.collaborators = await Promise.all(collaborators.map(e => e.format()))
 
-      ret.funding = this.get("funding")
-      ret.activities = this.get("activities")
+      // Format activities
+      const activities = this.get("activities")
+      if (activities && activities.length > 0) ret.activities = await Promise.all(activities.map(e => e.format()))
     } else {
       // Add recemt activity
       const recentActivity = findRecentActivity(this.get("activities"))
@@ -166,14 +168,52 @@ export const Project = Parse.Object.extend("Project", {
     // Include additional objects
     for (let index = 0; index < objects.length; index++) {
       query.include(objects[index])
-    }
-
+    }  
     const res = await query.find()
 
     return res[0]
   }
 })
 Parse.Object.registerSubclass('Project', Project);
+
+export const ProjectActivity = Parse.Object.extend("ProjectActivity", {
+  initialize: function (attrs) {
+    // Validate
+    if (!attrs) return
+    if (!attrs.project) throw new Error("Empty project")
+    if (!attrs.type || !variables.progress_type.includes(attrs.type)) throw new Error("Invalid project activity type")
+    if (!attrs.description || attrs.description === "") throw new Error("Empty project activity description")
+    if (!attrs.start_date) throw new Error("Empty project activity description")
+  },
+  format: function () {
+    let ret = {
+      id: this.id,
+      type: this.get("type"),
+      description: this.get("description"),
+      start_date: this.get("start_date")
+    }
+
+    const endDate = this.get("end_date")
+    if (endDate) ret.end_date = endDate
+    
+    return ret
+  }
+}, {
+  create: async function(attrs) {
+    // If it has ID, we just query
+    if (attrs.id) {
+      const query = new Parse.Query(ProjectActivity)
+      query.equalTo("objectId", attrs.id)
+      const result = await query.find()
+      if (result.length >= 1) return result[0] // If found, just return the target object
+      delete attrs.id // If not found, just drop id and proceed creating a new object
+    }
+    
+    // Create a new target object
+    return new ProjectActivity(attrs)
+  }
+})
+Parse.Object.registerSubclass('ProjectActivity', ProjectActivity);
 
 export async function addProject(projectInfo) {
   // Validate if logged in
@@ -216,13 +256,14 @@ export async function fetchTargets(limit, skip, filter) {
   if (filter.type !== '') query.equalTo("type", filter.type)
   if (filter.organism !== '') query.equalTo("organism", filter.organism)
   if (filter.name !== '') query.startsWith("name", filter.name)
+  query.exists("projects") // include only targets with projects associated
+  query.include("projects.team") // Include projects and team objects on the return
 
   // Apply pagination
   query.limit(limit)
   query.skip(skip)
   query.withCount() // include total amount of targets in the DB
-  query.exists("projects") // include only targets with projects associated
-  query.include("projects.team") // Include projects and team objects on the return
+
   let targets = await query.find()
   targets.results = await Promise.all(targets.results.map(e => e.format())) // Format targets
   
@@ -231,10 +272,8 @@ export async function fetchTargets(limit, skip, filter) {
 }
 
 export async function fetchProject(id, detail = false) {
-  // TODO: enforce ACL
-
   // Fetch project by ID
-  const project = await new Project.fetchById(id, ["target", "user", "team", "collaborators"])
+  const project = await new Project.fetchById(id, ["target", "user", "team", "collaborators", "activities"])
 
   // Format and return
   return project.format(detail)
@@ -250,6 +289,10 @@ export async function fetchProjectByTeamId(id, objects = []) {
   for (let index = 0; index < objects.length; index++) {
     query.include(objects[index])
   }
+
+  // Exlude fields
+  query.select(["target.type", "target.name", "target.organism", "features"])
+
   const projects = await query.find()
 
   // Format and return
@@ -270,13 +313,36 @@ export async function updateProject(payload) {
   const collaborators = await Promise.all(payload.collaborators.map(e => new Team.create({ id: e })))
   if (collaborators.length > 0 && collaborators.some(e => e.length < 1)) return new Error("Some collaborators are invalid")
 
+  // Construct activities
+  const activities = await Promise.all(payload.activities.map(async (e) => {
+    // Add project
+    e.project = project
+
+    const activity = await new ProjectActivity.create(e)
+    if (activity.id) {
+      activity.set("type", e.type)
+      activity.set("description", e.description)
+      activity.set("start_date", e.start_date)
+      if (e.end_date) activity.set("end_date", e.end_date)
+    }
+
+    return activity
+  }))
+
   // Update project
   project.set("leads", payload.leads)
   project.set("team", team)
   project.set("collaborators", collaborators)
   project.set("funding", payload.funding)
-  project.set("activities", payload.activities)
+  project.set("activities", activities)
+
+  // Set publication status. If it's true, then the current project will be shown to everyone.
+  // However, only the publication activity will be made available.
+  // Other activities are only available to followers
+  project.set("has_publication", payload.activities.filter(e => e.type === "Publication Available").length > 0)
 
   // Save project
-  return await project.save()
+  await project.save()
+
+  // TODO: Schedule a cloud job to remove no longer needed activities
 }
