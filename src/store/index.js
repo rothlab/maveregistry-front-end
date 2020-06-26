16232b2ab5bbe1b58ed383c9ca0af6d49e361a20
Kevin Kuang <kvnkuang@users.noreply.github.com>
2020-06-26 13:36:39 -0400

    Handle email validation error

--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,10 @@ Vue.use(Vuex)
 
 // Import Backend API
 import * as UserManage from "../api/userManage"
+import { handleError } from '../api/errorHandler';
+import { SnackbarProgrammatic as Snackbar } from 'buefy'
+import { ToastProgrammatic as Toast } from 'buefy'
+import { resendValidationEmail } from "@/api/userManage.js"
 
 export default new Vuex.Store({
   state: {
@@ -60,14 +64,54 @@ export default new Vuex.Store({
       commit('loginUser', user)
     },
     async signupLoginUserOrcid ({ commit }, userInfo) {
-      const user = await UserManage.signupLoginUserOrcid(userInfo)
+      try {
+        const user = await UserManage.signupLoginUserOrcid(userInfo)
+        
+        if (user && user.email) {
+          // Update user info if loggined in successfully
+          commit('loginUser', user)
+        }
 
-      if (user && user.email) {
-        // Update user info if loggined in successfully
-        commit('loginUser', user)
+        return user
+      } catch (e) {
+        // Handle email verfication failure
+        // This error needs to be handled separately because we need to provide user
+        // an option to resend verification email
+        const error = await handleError(e)
+
+        if (error instanceof Object && error.action === "resend_email") {
+          Snackbar.open({
+            message: error.message,
+            type: "is-danger",
+            position: "is-top",
+            queue: false,
+            actionText: "Resend Verification Email",
+            indefinite: true,
+            onAction: async () => {
+              try {
+                await resendValidationEmail(error.username)
+              } catch (err) {
+                Toast.open({
+                  message: await handleError(err),
+                  duration: 5000,
+                  type: 'is-danger'
+                })
+                return
+              }
+              
+              Toast.open({
+                message: "Verification email sent",
+                duration: 5000,
+                type: 'is-success'
+              })
+            }
+          })
+
+          return "resend_email"
+        } else {
+          throw e
+        }
       }
-
-      return user
     },
     async updateUserProfile ({ commit }, userInfo) {
       const user = await UserManage.updateUserProfile(userInfo)
