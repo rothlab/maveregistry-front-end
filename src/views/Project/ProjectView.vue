6f91c542602702c3e8f57bde8c22e4a2d1d68128
Kevin Kuang <kvnkuang@users.noreply.github.com>
2020-07-29 13:36:06 -0400

    Refactored transfer related function into a component

--- src/views/Project/ProjectView.vue
+++ src/views/Project/ProjectView.vue
@@ -340,62 +340,17 @@
               class="project-content"
               v-if="hasActions"
             >
-              <!-- Transfer ownership -->
-              <b-button
-                v-if="isOwner && !transfer"
-                type="is-light"
-                icon-left="mdil-repeat"
-                @click="isTransferOwnershipModalActive = true"
-                expanded
-              >
-                Transfer Ownership
-              </b-button>
-              <b-button
-                v-else-if="isOwner && transfer"
-                type="is-warning"
-                icon-left="mdil-repeat-off"
-                @click="isCancelTransferOwnershipModalActive = true"
-                expanded
-              >
-                Cancel Ownership Transfer
-              </b-button>
-              <b-button
-                v-else-if="hasLoggedIn && !isOwner && transfer"
-                type="is-warning"
-                icon-left="mdil-repeat"
-                @click="isReviewTransferOwnershipModalActive = true"
-                expanded
-              >
-                Review Ownership Transfer
-              </b-button>
+              <TransferAction
+                type="project"
+                :load-page="loadPage"
+                :is-owner="isOwner"
+                :target-id="projectId"
+                @has-transfer="(e) => hasTransfer = e"
+              />
             </div>
           </div>
         </div>
       </div>
-
-      <!-- Transfer ownership modal -->
-      <TransferOwnershipModal
-        :active.sync="isTransferOwnershipModalActive"
-        type="project"
-        :target-id="projectId"
-        @transfer="(e) => transfer = e"
-      />
-
-      <!-- Cancel ownership transfer modal -->
-      <CancelTransferModal
-        v-if="transfer"
-        :active.sync="isCancelTransferOwnershipModalActive"
-        :transfer="transfer"
-        @transfer="transfer = undefined; isCancelTransferOwnershipModalActive = false"
-      />
-
-      <!-- Cancel ownership transfer modal -->
-      <ReviewTransferModal
-        v-if="transfer"
-        :active.sync="isReviewTransferOwnershipModalActive"
-        :transfer="transfer"
-        @transfer="loadPage(); isReviewTransferOwnershipModalActive = false"
-      />
       
       <!-- Manage follower modal -->
       <ManageFollowerModal
@@ -412,14 +367,11 @@
 <script>
 import * as ProjectManage from "@/api/projectManage.js"
 import * as FollowManage from "@/api/followManage.js"
-import * as TransferManage from "@/api/transferManage.js"
 import { handleError } from "@/api/errorHandler.js"
 import Error from '@/components/Error.vue'
 import ManageFollowerModal from '@/components/Modal/ManageFollowerModal.vue'
 import FollowButtonAction from '@/components/Action/FollowButtonAction.vue'
-import TransferOwnershipModal from '@/components/Modal/TransferOwnershipModal.vue'
-import CancelTransferModal from '@/components/Modal/CancelTransferModal.vue'
-import ReviewTransferModal from '@/components/Modal/ReviewTransferModal.vue'
+import TransferAction from '@/components/Action/TransferAction.vue'
 
 export default {
   title: "View Project",
@@ -427,9 +379,7 @@ export default {
     Error,
     ManageFollowerModal,
     FollowButtonAction,
-    TransferOwnershipModal,
-    CancelTransferModal,
-    ReviewTransferModal
+    TransferAction
   },
   data() {
     return {
@@ -438,9 +388,6 @@ export default {
       },
       hasProject: false,
       isManageFollowerModalActive: false,
-      isTransferOwnershipModalActive: false,
-      isCancelTransferOwnershipModalActive: false,
-      isReviewTransferOwnershipModalActive: false,
       followStatus: undefined,
       isRequest: false,
       errorMessage: "",
@@ -455,7 +402,7 @@ export default {
       activities: [],
       followerCount: 0,
       requestCount: 0,
-      transfer: undefined
+      hasTransfer: true // Default set to true so that the trasfer action can load
     }
   },
   computed: {
@@ -469,7 +416,7 @@ export default {
       return this.activities.length > 0
     },
     hasActions() {
-      return !!this.transfer || this.isOwner
+      return !!this.hasTransfer || this.isOwner
     },
     isOwner() {
       return this.creator && this.creator.username && this.$store.getters.isOwner(this.creator.username)
@@ -482,8 +429,6 @@ export default {
 
     // Open follow request modal if needed
     if (this.hasDeepLink("#manage-request") && this.isOwner) this.openFollowerModal(true)
-    // Open review transfer modal
-    if (this.hasDeepLink("#review-transfer-request") && this.transfer) this.isReviewTransferOwnershipModalActive = true
 
     this.isLoading.page = false
   },
@@ -506,11 +451,6 @@ export default {
       if (this.isOwner) {
         // Fetch team follower and request count
         await this.fetchFollowerAndRequestCount(this.projectId)
-      }
-
-      if (this.hasLoggedIn) {
-        // Fetch pending transfer
-        await this.fetchTransfer()
       }
     },
     async fetchProject(id) {
@@ -550,17 +490,6 @@ export default {
     openFollowerModal(request) {
       this.isManageFollowerModalActive = true
       this.isRequest = request
-    },
-    hasDeepLink(action) {
-      return this.$route.hash === action
-    },
-    async fetchTransfer() {
-      const target = {
-        type: "project",
-        id: this.projectId
-      }
-
-      this.transfer = await TransferManage.fetchTransfer("owner", target, this.isOwner)
     }
   }
 }
