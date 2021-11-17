5c13a514cf426f0c8590e5e7085354383e35d98c
Kevin Kuang <kvnkuang@users.noreply.github.com>
2021-11-17 11:28:57 -0500

    Fix date filter for project list

--- src/api/projectManage.js
+++ src/api/projectManage.js
@@ -308,7 +308,7 @@ export async function fetchTargets(limit, skip, filter) {
   if (filter.type !== '') query.equalTo("type", filter.type)
   if (filter.organism !== '') query.equalTo("organism", filter.organism)
   if (filter.name !== '') query.startsWith("name", filter.name.toLowerCase())
-  if (filter.created_after) query.greaterThanOrEqualTo("updatedAt", filter.created_after) // Using update date because we want to include all targets
+  if (filter.created_after) query.greaterThanOrEqualTo("createdAt", filter.created_after)
   if (filter.conditions.length > 0) {
     const projectQuery = new Parse.Query(Project)
     const followedTargetQuery = new Parse.Query(Target)
